ed5f3890f2045e1b4ea034ba8c52bf35227e9dfb
jojoscholzgmailcom <jojo.scholz@gmail.com>
2020-12-23 20:58:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var output = document.getElementById("value");
 var outerRing = new Image();
 var val = 0;
 outerRing.onload=function(){
-    drawRotatedImage(outerRing,c.width*0.5,c.height*0.5,c.width,c.height, 5- val*10);
+    drawRotatedImage(outerRing,250, 250,c.width,c.height, 5- val*10);
 };
 outerRing.src="wheel.png";
 var innerRing = new Image();
 innerRing.onload=function(){
-    drawRotatedImage(innerRing,c.width*0.5, c.height*0.5, c.width*0.75, c.height*0.75, 5);
+    drawRotatedImage(innerRing,250, 250, c.width*0.75, c.height*0.75, 5);
 };
 innerRing.src="wheel.png";
 var TO_RADIANS = Math.PI/180; 
@@ -26,6 +26,6 @@ slider.oninput = function() {
     ctx.clearRect(0,0,c.width,c.height);
     val = this.value;
     output.innerHTML = this.value;
-    drawRotatedImage(outerRing,c.width*0.5, c.height*0.5,c.width,c.height, 5-this.value*13.84615);
-    drawRotatedImage(innerRing,c.width*0.5, c.height*0.5, c.width*0.75, c.height*0.75, 5);
+    drawRotatedImage(outerRing,250, 250,c.width,c.height, 5-this.value*13.84615);
+    drawRotatedImage(innerRing,250, 250, c.width*0.75, c.height*0.75, 5);
 }
